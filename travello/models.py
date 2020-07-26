4b33f572c1a1dbfc4b925c59c8c57bf19f0190ed
rajendra <rajendrasinghrana79@gmail.com>
2020-07-26 08:52:38 +0530

    accounts section

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    price = models.FloatField(null=False)
+    image = models.ImageField(upload_to='pic')
+    offer = models.BooleanField(default=False)
--- travello/views.py
+++ travello/views.py
@@ -1,8 +1,18 @@
+import os
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
+from .models import Destination
 # Create your views here.
 
 
 def index(request):
-    name = ["Rajendra", "naveen", "kiran"]
-    return render(request, 'index.html', {'name': name})
+    dest = Destination.objects.all()
+    return render(request, 'index.html', {'destination': dest})
+
+
+def favicon(request):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    imagepath = os.path.join(BASE_DIR, 'static/images/about.jpg')
+    img = open(imagepath)
+    response = FileResponse(img)
+    return HttpResponse(request, imagepath)
